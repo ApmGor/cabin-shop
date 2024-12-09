fa4eae6d9a08155d8db0c15426d268575b470eeb
Artur <apmgor@gmail.com>
2024-12-09 19:49:23 +0300

    Arch and Design changes

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,14 +1,10 @@
-import Counter from "@/app/components/Counter";
+export const metadata = {
+  title: "Cabins"
+}
 
-export default async function Page() {
+export default function Page() {
 
-  const res = await fetch("https://jsonplaceholder.typicode.com/users");
-  const data = await res.json();
   return <div>
     <h1>Cabins Page</h1>
-    <ul>
-      {data.map(user => <li key={user.id}>{user.name}</li>)}
-    </ul>
-    <Counter users={data} />
   </div>;
 }
