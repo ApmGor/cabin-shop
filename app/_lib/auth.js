7229ccadd992d2fadce09f6466fa3fd0bed862b5
Artur <apmgor@gmail.com>
2024-12-16 05:07:25 +0300

    Define auth callbacks

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,11 +1,26 @@
 import NextAuth from "next-auth";
 import Google from "next-auth/providers/google";
+import {createGuest, getGuest} from "@/app/_lib/data-service";
 
 const authConfig = {
   providers: [Google],
   callbacks: {
     authorized({auth, request}) {
-      return !!auth?.user;
+      return !!auth;
+    },
+    async signIn({user, account, profile}) {
+      try {
+        const existingGuest = await getGuest(user.email);
+        if (!existingGuest) await createGuest({email: user.email, fullName: user.name});
+        return true;
+      } catch {
+        return false;
+      }
+    },
+    async session({session, token}){
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     }
   },
   pages: {
